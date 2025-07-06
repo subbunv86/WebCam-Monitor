21d2527976f83d6043c93a33b3c03dae290b1e67
subbunv86 <subbunv86@gmail.com>
2025-07-06 14:23:11 +0530

    With threads

--- emailing.py
+++ emailing.py
@@ -1,14 +1,38 @@
-def send_email():
-    """
-    Sends an email with the specified subject and body to the given recipient.
-    
-    Args:
-        to (str): The recipient's email address.
-        subject (str): The subject of the email.
-        body (str): The body content of the email.
-    """
+import smtplib,ssl
+import os
+from email.message import EmailMessage
 
+host="smtp.gmail.com"
+port=587
+username="subbu.venkat86@gmail.com"
+paswword="eoux uydp pwzb pmhp"
+
+receiver_email="srividya.srinivas88@gmail.com"
+context = ssl.create_default_context()
+
+
+def send_email(image_path,Message,thread=None):
     print("Sending email...")
-    #print(f"Sending email to {to} with subject '{subject}' and body:\n{body}")
-    # Here you would implement the actual sending logic using an email library
-    # For example, using smtplib in Python
+    
+    if not os.path.exists(image_path):
+        print(f"Image path {image_path} does not exist.")
+        return
+    
+    email_message = EmailMessage()
+    email_message['Subject'] = 'Movement Detected'  
+    email_message.set_content(Message)
+    email_message.add_attachment(open(image_path, 'rb').read(), maintype='image', subtype=os.path.splitext(image_path)[1][1:], filename=os.path.basename(image_path))   
+
+    gmail = smtplib.SMTP(host, port)
+    gmail.ehlo()
+    gmail.starttls(context=context)
+    gmail.login(username, paswword)
+    gmail.sendmail(username,receiver_email, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email("images/image.png", "Movement detected, see the attached image.")
+
+    # with smtplib.SMTP_SSL(host,port,context=context) as server:
+    #     server.login(username,paswword)
+    #     server.sendmail(username,receiver_email,Message)
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import cv2
+import os
 import time
 from emailing import send_email
+from threading import Thread
+
+import glob
+
 video = cv2.VideoCapture(0)
 time.sleep(2)  # Allow camera to warm up
 first_frame = None
 status_list = []
+count =1
+
+def cleanup_images(thread=None):
+    thread.join()
+    print("Cleaning up images...")
+    all_images = glob.glob("images/*.png")
+    for image in all_images:
+        os.remove(image)
+    
+    count = 1
+
 while True:
     status =0
     time.sleep(1)
     check, frame = video.read()
+    
+    
     gray_frame= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau =cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
@@ -30,22 +48,38 @@ while True:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 13)
+        
         if rectangle.any():
             status = 1
-            send_email()
+            cv2.imwrite(f"images\\frame{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2 )
+            image_tosend = all_images[index]
+
+            
     status_list.append(status)
     if len(status_list) > 2:
         status_list = status_list[-2:]
-    if status_list[-1] == 1 and status_list[-2] == 0:
+
+    if status_list[0] == 1 and status_list[1] == 0:
         print("Movement detected")
+        email_thread = Thread(target=send_email, args=(image_tosend, "See your face movement detected"))
+        email_thread.daemon = True
+
+        clean_thread = Thread(target=cleanup_images, args=(email_thread,))
+        clean_thread.daemon = True
+       
         # Here you can call the function to send an email
-        send_email()
-        
+        email_thread.start()
+        clean_thread.start()
+
     cv2.imshow("Capturing", frame)
     
 
     key = cv2.waitKey(1)
     if key == ord('q'):
+        
         break
 
 video.release()
